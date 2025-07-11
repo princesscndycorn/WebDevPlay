b76b6c87bc8f5e74744a5216cb8a77f4ebcdedea
Nam-shub <rob.mcmillen@gmail.com>
2025-07-11 12:51:10 -0700

    Update tasks.py

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -13,19 +13,19 @@ app = Celery(
     include=['tasks']
 )
 
-# app.conf.broker_use_ssl = {
-#     'ssl_cert_reqs': 'required',
-#     'ssl_ca_certs': '/app/redis.crt',
-#     'ssl_certfile': '/app/client.crt',
-#     'ssl_keyfile': '/app/client.key',
-# }
-
 app.conf.broker_use_ssl = {
-    'ssl_certfile': os.environ.get("REDIS_CLIENT_CERT"),
-    'ssl_keyfile': os.environ.get("REDIS_CLIENT_KEY"),
-    'ssl_ca_certs': os.environ.get("REDIS_CA_CERT"),
     'ssl_cert_reqs': 'none',
+    'ssl_ca_certs': '/app/redis.crt',
+    'ssl_certfile': '/app/client.crt',
+    'ssl_keyfile': '/app/client.key',
 }
+
+# app.conf.broker_use_ssl = {
+#     'ssl_certfile': os.environ.get("REDIS_CLIENT_CERT"),
+#     'ssl_keyfile': os.environ.get("REDIS_CLIENT_KEY"),
+#     'ssl_ca_certs': os.environ.get("REDIS_CA_CERT"),
+#     'ssl_cert_reqs': 'require',
+# }
 app.conf.redis_backend_use_ssl = app.conf.broker_use_ssl
 
 
